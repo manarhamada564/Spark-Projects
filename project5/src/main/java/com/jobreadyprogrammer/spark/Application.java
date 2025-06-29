9eb8b01e6756bc284b8cd78389df053d3dbadff7
Manar Hamada Elsayed <74185394+Manar20575@users.noreply.github.com>
2025-06-29 18:41:19 +0300

    Joining&Transformation

---
+++ project5/src/main/java/com/jobreadyprogrammer/spark/Application.java
@@ -0,0 +1,53 @@
+package com.jobreadyprogrammer.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		SparkSession spark = SparkSession.builder()
+		        .appName("Learning Spark SQL Dataframe API")
+		        .master("local")
+		        .getOrCreate();
+		
+		
+		 String studentsFile = "src/main/resources/students.csv";
+		 
+		    Dataset<Row> studentDf = spark.read().format("csv")
+		        .option("inferSchema", "true")
+		        .option("header", true)
+		        .load(studentsFile); 
+		    
+		 String gradeChartFile = "src/main/resources/grade_chart.csv";
+		 
+		    Dataset<Row> gradesDf = spark.read().format("csv") 
+		        .option("inferSchema", "true") // Make sure to use string version of true
+		        .option("header", true)
+		        .load(gradeChartFile);
+		    
+//		    studentDf.join(gradesDf, studentDf.col("GPA").equalTo(gradesDf.col("gpa")))
+//		    	.select(studentDf.col("student_name"),
+//		    			studentDf.col("favorite_book_title"),
+//		    			gradesDf.col("letter_grade")).show();
+//
+//		    
+		    //do filter first 
+		    Dataset<Row> filteredDf = studentDf.join(gradesDf, studentDf.col("GPA").equalTo(gradesDf.col("gpa")))
+		    	.filter(gradesDf.col("gpa").gt(3.0).and(gradesDf.col("gpa").lt(4.5))
+		    									  .or(gradesDf.col("gpa").equalTo(1.0)))
+		    	.select("student_name",
+		    			"favorite_book_title",
+		    			"letter_grade");
+		    
+		    filteredDf.show();
+		    
+	}
+
+	
+	
+}
